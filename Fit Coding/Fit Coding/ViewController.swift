84cc5c82b259ea46b7540e4ae7337ee807eaa0df
LowikBourgeoisat <lowik.bourgeoisat@edu.itescia.fr>
2021-01-08 10:50:54 +0100

    API Call

--- Fit Coding/Fit Coding/ViewController.swift
+++ Fit Coding/Fit Coding/ViewController.swift
@@ -11,9 +11,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let url = "https://api.spoonacular.com/recipes/716429/information?apiKey=0a244cc1f5704bc8ad9d876a898283a2&includeNutrition=false"
+        getData(from: url)
     }
+    
+    private func getData(from url: String) {
+        let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
+            
+            guard let data = data, error == nil else {
+                print("something went wrong")
+                return
+            }
+            var result: Response?
+            do {
+                result = try JSONDecoder().decode(Response.self, from: data)
+            }
+            catch {
+                print("failed to convert \(error)")
+            }
+            guard let json = result else {
+                return
+            }
+            print(json.extendedIngredients)
+        })
+        
+        task.resume()
+    }
+}
 
+struct Response: Codable {
+    let extendedIngredients: [Data]
+        
+}
 
+struct Data: Codable {
+    let name: String
+    
 }
 
